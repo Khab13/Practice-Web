5cc7cf095979776e767ab1e7109875af5e4fc90d
Habib Muhabbatov <khabbaiph@gmail.com>
2023-11-11 23:30:36 +0500

    minify js and css

--- js/main-es6.js
+++ js/main-es6.js
@@ -1,11 +1,11 @@
 (function () {
     const header = document.querySelector('.header');
     window.onscroll = () => {
-    if (window.pageYOffset > 50) {
-        header.classList.add('header__active')
-    } else {
-        header.classList.remove('header__active')
-    }
+        if (window.pageYOffset > 50) {
+            header.classList.add('header__active')
+        } else {
+            header.classList.remove('header__active')
+        }
     }
 }());
 
@@ -15,37 +15,45 @@
     const burgerItem = document.querySelector('.burger');
     const menu = document.querySelector('.header__nav');
     const menuCloseItem = document.querySelector('.header__nav-close');
+    const menuLinks = document.querySelectorAll('.header__link');
     burgerItem.addEventListener('click', () => {
         menu.classList.add('header__nav-active');
     })
     menuCloseItem.addEventListener('click', () => {
         menu.classList.remove('header__nav-active');
     })
+    if (window.innerWidth <= 767) {
+        menuLinks.forEach((link) => {
+            link.addEventListener('click', () => {
+                menu.classList.remove('header__nav-active');
+            })
+        })
+    }
 }());
 
 // Scroll to anchors
 (function () {
 
     const smoothScroll = function (targetEl, duration) {
-        const headerElHeight =  document.querySelector('.header').clientHeight;
+        const headerElHeight = document.querySelector('.header').clientHeight;
         console.log(headerElHeight);
         let target = document.querySelector(targetEl);
         let targetPosition = target.getBoundingClientRect().top - headerElHeight;
         let startPosition = window.pageYOffset;
         let startTime = null;
-    
-        const ease = function(t,b,c,d) {
+
+        const ease = function (t, b, c, d) {
             t /= d / 2;
             if (t < 1) return c / 2 * t * t + b;
             t--;
             return -c / 2 * (t * (t - 2) - 1) + b;
         };
-    
-        const animation = function(currentTime){
+
+        const animation = function (currentTime) {
             if (startTime === null) startTime = currentTime;
             const timeElapsed = currentTime - startTime;
             const run = ease(timeElapsed, startPosition, targetPosition, duration);
-            window.scrollTo(0,run);
+            window.scrollTo(0, run);
             if (timeElapsed < duration) requestAnimationFrame(animation);
         };
         requestAnimationFrame(animation);
